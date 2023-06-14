e0ee23d4e97d2c4b90bff27678098d8c2cc2f37a
Jeanmaire dit cartier <yann.jeanmaire@gmail.com>
2023-06-14 08:38:19 +0200

    Initial commit

---
+++ src/main/java/com/example/aoo/AooApplication.java
@@ -0,0 +1,13 @@
+package com.example.aoo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AooApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AooApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/aoo/AooApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.aoo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AooApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
